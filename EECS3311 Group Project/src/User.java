6cb4f6f9aa2c9e84ff11c91ed103d5242b792d05
Evan3636 <60149454+Evan3636@users.noreply.github.com>
2023-02-24 19:25:19 -0500

    Update User.java

--- EECS3311 Group Project/src/User.java
+++ EECS3311 Group Project/src/User.java
@@ -4,13 +4,31 @@ public class User {
 
 	private String name;
 	private String password;
+	private String regionLevel;
 	private List<String> selectedRegions = new ArrayList<String>();
+	private List<String> selectedVisualizations = new ArrayList<String>();
 
+	public void setName(String name){
+		this.name = name;
+	}
+	
 	public String getName() {
 		return this.name;
 	}
 	
+	public void setRegionLevel(String level){
+		regionLevel=level;
+	}
+	
 	public List<String> getSelectedRegions() {
 		return this.selectedRegions;
+	}
+	
+	public List<String> getVisualizations() {
+		return this.selectedVisualizations;
+	}
+	
+	public void selectVisualization(String visual){
+		selectedVisualizations.add(visual);
 	}
 }
