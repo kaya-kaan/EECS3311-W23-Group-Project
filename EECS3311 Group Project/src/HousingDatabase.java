cd27351b7180e6751a4e7f4cde622559e2077057
Evan3636 <60149454+Evan3636@users.noreply.github.com>
2023-03-27 17:17:09 -0400

    Update HousingDatabase.java

--- EECS3311 Group Project/src/HousingDatabase.java
+++ EECS3311 Group Project/src/HousingDatabase.java
@@ -3,15 +3,27 @@ public class HousingDatabase {
 
 	private HashMap<String, List<HousingPrice>> data = new HashMap<String, List<HousingPrice>>();
 	
-	public HashMap<String, List<HousingPrice>> getData(){
-		
-		return ;
+	public void addPrice(HousingPrice price){
+		data.put(price.name, price);
 	}
 	
-	public List<HousingPrice> getPriceForLocation(String location){
+	//Retrieves list of housing prices of a specified location during a specific time span.
+	public HashMap<String, List<HousingPrice>> getCityPrices(String location, LocalDate startDate, LocalDate endDate,){	
+		LocalDate date = startDate;
+		List<HousingPrice> list = new ArrayList<String>();	//list is the list of all housing prices for the specific location.
+		List<HousingPrice> priceList = new ArrayList<String>();	//priceList is the return list.
+		int index = 0;
 		
-		return ;
+		list = data.get(location);
+		
+		//loop through list, and add items to priceList if the date is within the threshold.
+		while (index < list.size() || list.get(index).date < endDate){
+			if (list.get(index).date >= startDate && list.get(index).date <= endDate){
+				priceList.add(list.get(index));
+			}
+			index++;
+		}
+		return priceList;
 	}
-	
-	
+
 }
