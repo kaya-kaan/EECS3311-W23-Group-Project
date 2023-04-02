01021b6a2bc4dae4432a1f34fd58c964a7f27184
Amir Montazeri <77297937+montazeri-amir@users.noreply.github.com>
2023-04-02 18:15:00 -0400

    Update HousingDatabase.java
    This will get data from ProjectDB and search for a city or province and return all data from that.

--- EECS3311 Group Project/src/HousingDatabase.java
+++ EECS3311 Group Project/src/HousingDatabase.java
@@ -1,32 +1,62 @@
-import java.util.*;
-public class HousingDatabase {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-	private HashMap<String, List<HousingPrice>> data;
-	public void HousingDatabase(){
-		data = new HashMap<String, List<HousingPrice>>();
-	}
-	
-	public void addPrice(HousingPrice price){
-		data.put(price.name, price);
-	}
-	
-	//Retrieves list of housing prices of a specified location during a specific time span.
-	public HashMap<String, List<HousingPrice>> getCityPrices(String location, LocalDate startDate, LocalDate endDate,){	
-		LocalDate date = startDate;
-		List<HousingPrice> list = new ArrayList<String>();	//list is the list of all housing prices for the specific location.
-		List<HousingPrice> priceList = new ArrayList<String>();	//priceList is the return list.
-		int index = 0;
-		
-		list = data.get(location);
-		
-		//loop through list, and add items to priceList if the date is within the threshold.
-		while (index < list.size() || list.get(index).date < endDate){
-			if (list.get(index).date >= startDate && list.get(index).date <= endDate){
-				priceList.add(list.get(index));
-			}
-			index++;
-		}
-		return priceList;
-	}
+public class SimpleCSVToHashMap {
 
+    public static void main(String[] args) {
+        String csvFileName = "ProjectDB.csv";
+
+        try {
+            String csvFilePath = Paths.get("").toAbsolutePath().resolve(csvFileName).toString();
+            Map<String, String[]> dataMap = readCSVDataToHashMap(csvFilePath);
+            
+            for (Map.Entry<String, String[]> entry : dataMap.entrySet()) {
+            	//if (entry.getValue()[1].contains("Ontario")) {
+            	if (valueContainsString(dataMap, entry.getKey(), "Ontario")) { // we have to change the Ontario to user's input
+            		System.out.println("Key: " + entry.getKey() + ", Value: " + Arrays.toString(entry.getValue()));
+            	}
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static boolean valueContainsString(Map<String, String[]> dataMap, String key, String searchString) {
+        if (!dataMap.containsKey(key)) {
+            return false;
+        }
+
+        String[] values = dataMap.get(key);
+        for (String value : values) {
+            if (value.contains("Quebec part")) {
+            	return false;
+            } else if (value.contains(searchString)) {
+            	return true;
+            }
+        }
+        return false;
+    }
+
+    private static Map<String, String[]> readCSVDataToHashMap(String csvFilePath) throws IOException {
+        Map<String, String[]> dataMap = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+            br.readLine(); // Skip header line
+
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                String key = row[0];
+                String[] value = Arrays.copyOfRange(row, 1, row.length);
+                dataMap.put(key, value);
+            }
+        }
+
+        return dataMap;
+    }
 }
