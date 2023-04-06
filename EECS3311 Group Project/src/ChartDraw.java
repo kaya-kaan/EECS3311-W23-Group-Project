b7b075f70181903368bd43a0b3c04363ed64cd83
Amir Montazeri <77297937+montazeri-amir@users.noreply.github.com>
2023-04-06 17:54:52 -0400

    Update ChartDraw.java

--- EECS3311 Group Project/src/ChartDraw.java
+++ EECS3311 Group Project/src/ChartDraw.java
@@ -1,28 +1,36 @@
 package project;
 
 import java.awt.Dimension;
-import java.util.HashMap;
+import java.awt.Font;
+import java.util.ArrayList;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.AbstractCategoryItemRenderer;
+import org.jfree.chart.renderer.category.ScatterRenderer;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.xy.DefaultXYDataset;
-import org.jfree.data.xy.XYDataset;
+import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
+import org.jfree.data.statistics.MultiValueCategoryDataset;
 
-//import project.Backend;
+
 
 public class ChartDraw extends JFrame {
 	
 	//Map<String, Double> data = Backend.getDataToDraw();
 
-    public ChartDraw(String chartType, HashMap<String, Double> data) {
+    public ChartDraw(String chartType, LinkedHashMap<String, Double> data) {
         JPanel chartPanel;
         switch (chartType) {
             case "bar":
@@ -37,100 +45,110 @@ public class ChartDraw extends JFrame {
                 break;
         }
 
-        chartPanel.setPreferredSize(new Dimension(800, 600));
+        chartPanel.setPreferredSize(new Dimension(5000, 1125));
         setContentPane(chartPanel);
     }
 
 
 
-	private JPanel createLineChartPanel(HashMap<String, Double> data) {
+	private JPanel createLineChartPanel(LinkedHashMap<String, Double> data) {
         String chartTitle = "Line Graph Representation";
-        String categoryAxisLabel = "Key";
+        String categoryAxisLabel = "Date";
         String valueAxisLabel = "Value";
 
         CategoryDataset dataset = createCategoryDataset(data);
 
         JFreeChart chart = ChartFactory.createLineChart(chartTitle, categoryAxisLabel, valueAxisLabel,
                 dataset, PlotOrientation.VERTICAL, true, true, false);
+        
+        // Get the plot and category axis
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        CategoryAxis xAxis = plot.getDomainAxis();
+        
+        // Set the font size and rotation for the x-axis labels
+        Font currentFont = xAxis.getTickLabelFont();
+        Font newFont = new Font(currentFont.getFontName(), currentFont.getStyle(), 10);
+        xAxis.setTickLabelFont(newFont);
+        xAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 2.0));
+
 
         return new ChartPanel(chart);
     }
 
-    private JPanel createBarChartPanel(HashMap<String, Double> data) {
+    private JPanel createBarChartPanel(LinkedHashMap<String, Double> data) {
         String chartTitle = "Bar Graph Representation";
-        String categoryAxisLabel = "Key";
+        String categoryAxisLabel = "Date";
         String valueAxisLabel = "Value";
 
         CategoryDataset dataset = createCategoryDataset(data);
 
         JFreeChart chart = ChartFactory.createBarChart(chartTitle, categoryAxisLabel, valueAxisLabel,
                 dataset, PlotOrientation.VERTICAL, true, true, false);
+        
+        // Get the plot and category axis
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        CategoryAxis xAxis = plot.getDomainAxis();
+        
+        // Set the font size and rotation for the x-axis labels
+        Font currentFont = xAxis.getTickLabelFont();
+        Font newFont = new Font(currentFont.getFontName(), currentFont.getStyle(), 10);
+        xAxis.setTickLabelFont(newFont);
+        xAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 2.0));
 
         return new ChartPanel(chart);
     }
 
-    private JPanel createScatterPlotPanel(HashMap<String, Double> data) {
-        String chartTitle = "Scatter Plot Representation";
-        String xAxisLabel = "Key";
+    private JPanel createScatterPlotPanel(LinkedHashMap<String, Double> data) {
+    	String chartTitle = "Scatter Plot Representation";
+        String xAxisLabel = "Date";
         String yAxisLabel = "Value";
 
-        XYDataset dataset = createXYDataset(data);
+        MultiValueCategoryDataset dataset = createMultiCategoryDataset(data);
 
-        JFreeChart chart = ChartFactory.createScatterPlot(chartTitle, xAxisLabel, yAxisLabel,
+        JFreeChart chart = ChartFactory.createLineChart(chartTitle, xAxisLabel, yAxisLabel,
                 dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+
+        // Customize the renderer to create a scatter plot effect
+        AbstractCategoryItemRenderer renderer = new ScatterRenderer();
+        plot.setRenderer(renderer);
+
+        CategoryAxis xAxis = plot.getDomainAxis();
+
+        // Set the font size and rotation for the x-axis labels
+        Font currentFont = xAxis.getTickLabelFont();
+        Font newFont = new Font(currentFont.getFontName(), currentFont.getStyle(), 10);
+        xAxis.setTickLabelFont(newFont);
+        xAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 2.0));
 
         return new ChartPanel(chart);
     }
 
-    private CategoryDataset createCategoryDataset(HashMap<String, Double> data) {
+    private CategoryDataset createCategoryDataset(LinkedHashMap<String, Double> data) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
         for (Map.Entry<String, Double> entry : data.entrySet()) {
             dataset.addValue(entry.getValue(), "Date", entry.getKey());
-            System.out.println(dataset.getValue("Date", entry.getKey().toString()));
+            //System.out.println(dataset.getValue("Date", entry.getKey().toString()));
         }
 
         return dataset;
     }
-
-    private XYDataset createXYDataset(HashMap<String, Double> data) {
-        DefaultXYDataset dataset = new DefaultXYDataset();
-        double[][] seriesData = new double[2][data.size()];
-        int i = 0;
+    
+    private MultiValueCategoryDataset createMultiCategoryDataset(LinkedHashMap<String, Double> data) {
+        DefaultMultiValueCategoryDataset dataset = new DefaultMultiValueCategoryDataset();
 
         for (Map.Entry<String, Double> entry : data.entrySet()) {
-            seriesData[0][i] = Double.parseDouble(entry.getKey());
-            seriesData[1][i] = entry.getValue();
-            i++;
+            List<Double> values = new ArrayList<>();
+            values.add(entry.getValue());
+            dataset.add(values, "Date", entry.getKey());
         }
 
-        dataset.addSeries("Date", seriesData);
         return dataset;
     }
+    
 
-//    public static void main(String[] args) {
-//        HashMap<String, Double> hashMap = new HashMap<>();
-//        hashMap.put("1", 3.0);
-//        hashMap.put("2", 7.0);
-//        hashMap.put("3", 5.0);
-//        hashMap.put("4", 8.0);
-//        hashMap.put("5", 4.0);
-//
-//        ChartDraw lineChart = new ChartDraw("line", hashMap);
-//        lineChart.pack();
-//        lineChart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        lineChart.setVisible(true);
-//
-//        ChartDraw barChart = new ChartDraw("bar", hashMap);
-//        barChart.pack();
-//        barChart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        barChart.setVisible(true);
-//
-//        ChartDraw scatterPlot = new ChartDraw("scatter", hashMap);
-//        scatterPlot.pack();
-//        scatterPlot.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        scatterPlot.setVisible(true);
-//    }
 }
 
 
