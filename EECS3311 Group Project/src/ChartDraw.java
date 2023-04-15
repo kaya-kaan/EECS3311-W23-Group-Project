57656fd5f5525a224e94546c672ea48e0e64312f
Kaan Kaya <70816118+kaankaya7@users.noreply.github.com>
2023-04-15 15:36:18 -0400

    Graph types are now selectable after the chart is launched

--- EECS3311 Group Project/src/ChartDraw.java
+++ EECS3311 Group Project/src/ChartDraw.java
@@ -1,13 +1,22 @@
 
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
@@ -23,32 +32,94 @@ import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
 import org.jfree.data.statistics.MultiValueCategoryDataset;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+
+import java.text.NumberFormat;
+import javax.swing.AbstractAction;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.Year;
+import org.jfree.data.xy.XYDataset;
 
 
-
-public class ChartDraw extends JFrame {
+public class ChartDraw extends JFrame implements ActionListener{
+	
 	
 	//Map<String, Double> data = Backend.getDataToDraw();
 
-    public ChartDraw(String chartType, LinkedHashMap<String, Double> data) {
-        JPanel chartPanel;
-        switch (chartType) {
-            case "bar":
-                chartPanel = createBarChartPanel(data);
-                break;
-            case "scatter":
-                chartPanel = createScatterPlotPanel(data);
-                break;
-            case "line":
-            default:
-                chartPanel = createLineChartPanel(data);
-                break;
-        }
+    static String currentGraph;
+    private JPanel chartPanel;
+    private LinkedHashMap<String, Double> currData = new LinkedHashMap<String, Double>();
 
-        chartPanel.setPreferredSize(new Dimension(5000, 1125));
-        setContentPane(chartPanel);
+	public ChartDraw(String chartType, LinkedHashMap<String, Double> data) {
+        JFrame frame = new JFrame();
+        createChart(chartType, data);
+        currData = data;
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout(0, 5));
+        frame.add(chartPanel, BorderLayout.CENTER);
+        
+        JPanel panel = new JPanel (new FlowLayout(FlowLayout.RIGHT));
+        panel.add(chooseGraphType());
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
     }
+	
+	private void createChart(String chartType, LinkedHashMap<String, Double> data) {
+		
+		switch (chartType) {
+        case "bar":
+        	this.currentGraph = "bar";
+            chartPanel = createBarChartPanel(data);
+            break;
+        case "scatter":
+        	this.currentGraph = "scatter";
+            chartPanel = createScatterPlotPanel(data);
+            break;
+        case "line":
+        default:
+        	this.currentGraph = "line";
+            chartPanel = createLineChartPanel(data);
+            break;
+            
+            
+    }
+// Opens an unnecessary window. Could be used again if switching between graphs is not necessary. 
+//    chartPanel.add(chooseGraphType());
+//    chartPanel.setPreferredSize(new Dimension(1280, 720));
+//    setContentPane(chartPanel);
+	}
 
+    private JComboBox chooseGraphType() {
+        final JComboBox chooseGraph = new JComboBox();
+        final String[] graphTypes = {"Scatter Plot", "Bar Graph", "Line Graph"};
+        chooseGraph.setModel(new DefaultComboBoxModel(graphTypes));
+        chooseGraph.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (graphTypes[0].equals(currentGraph)) {
+                	ChartDraw scatterDraw = new ChartDraw("scatter", currData);
+                	//createChart("scatter", currData);
+                } else if (graphTypes[1].equals(currentGraph)){
+                	ChartDraw barDraw = new ChartDraw("bar", currData);
+                	//createChart("bar", currData);
+                } else {
+                	ChartDraw lineDraw = new ChartDraw("line", currData);
+                	//createChart("line", currData);
+                }
+            }
+        });
+        return chooseGraph;
+    }
 
 
 	private JPanel createLineChartPanel(LinkedHashMap<String, Double> data) {
@@ -70,7 +141,6 @@ public class ChartDraw extends JFrame {
         Font newFont = new Font(currentFont.getFontName(), currentFont.getStyle(), 10);
         xAxis.setTickLabelFont(newFont);
         xAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 2.0));
-
 
         return new ChartPanel(chart);
     }
@@ -121,7 +191,7 @@ public class ChartDraw extends JFrame {
         Font newFont = new Font(currentFont.getFontName(), currentFont.getStyle(), 10);
         xAxis.setTickLabelFont(newFont);
         xAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 2.0));
-
+        
         return new ChartPanel(chart);
     }
 
@@ -147,6 +217,13 @@ public class ChartDraw extends JFrame {
 
         return dataset;
     }
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
     
 
 }
