e62cb045f46c605d210b418c10286793109ca971
Evan3636 <60149454+Evan3636@users.noreply.github.com>
2023-04-15 20:20:32 -0400

    Update PriceTable.java

--- EECS3311 Group Project/src/PriceTable.java
+++ EECS3311 Group Project/src/PriceTable.java
@@ -1,10 +1,16 @@
-package src;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -15,11 +21,84 @@ public class PriceTable extends JFrame {
     private JTable table;
     private DefaultTableModel model;
 
+    private JComboBox chooseTableType(int Type) {
+        final JComboBox chooseTable = new JComboBox();
+        final String[] tableTypes = {"Choose Table Type","Detailed", "Raw Data"};
+        chooseTable.setModel(new DefaultComboBoxModel(tableTypes));
+        chooseTable.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (tableTypes[1].equals(chooseTable.getSelectedItem())) {
+                	if(Type == 1) {
+                		setVisible(false);
+                	}
+                } else if (tableTypes[2].equals(chooseTable.getSelectedItem())){
+                	if(Type == 0) {
+                		setVisible(false);
+                	}
+                }
+            }
+        });
+        return chooseTable;
+    }
+    
+    public PriceTable(String name,String nhpi,LinkedHashMap<String, Double> data) {
+    	JFrame frame = new JFrame();
+    	setTitle("Raw Data Table");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Set up the table model
+        String[] columnNames = {"Date", "Location", "Data Type", "Price Value"};
+        model = new DefaultTableModel(columnNames, 0);
+
+        // Fill the table model with data for each location
+        
+            String locationName = name;
+            List<String> DateList = new ArrayList<String>(data.keySet());
+            List<Double> ValueList = new ArrayList<Double>(data.values());
+            
+            int i=0;
+            double minPrice = Double.MAX_VALUE;
+            double maxPrice = Double.MIN_VALUE;
+            double sumPrices = 0.0;
+
+            // Calculate min, max, and sum of prices
+            for (String date : DateList) {
+            	
+             	
+	            // Add a row to the table model for the location
+	            Object[] rowData = {date, locationName, nhpi, ValueList.get(i)};
+	            model.addRow(rowData);
+	            i++;
+            }
+        
+
+        // Set up the JTable with the table model
+        table = new JTable(model);
+
+        // Add the JTable to a JScrollPane and add the scroll pane to the JFrame
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane);
+        
+        JPanel panel = new JPanel (new FlowLayout(FlowLayout.RIGHT));
+        panel.add(chooseTableType(1));
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.pack();
+		frame.setLocationRelativeTo(null);
+
+        // Make the JFrame visible
+        setVisible(true);
+    }
+    
+    
     public PriceTable(List<String>names, List<Double>... prices) {
+    	JFrame frame = new JFrame();
         // Set up the JFrame
         setTitle("Price Table");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(200, 100);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         // Set up the table model
         String[] columnNames = {"Location", "Average Price", "Min Price", "Max Price", "Standard Deviation"};
@@ -72,46 +151,15 @@ public class PriceTable extends JFrame {
         // Set up the JTable with the table model
         table = new JTable(model);
 
-        // Add the JTable to a JScrollPane and add the scroll pane to the JFrame
         JScrollPane scrollPane = new JScrollPane(table);
         add(scrollPane);
-
-        // Make the JFrame visible
+        
+        JPanel panel = new JPanel (new FlowLayout(FlowLayout.RIGHT));
+        panel.add(chooseTableType(0));
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.pack();
+		frame.setLocationRelativeTo(null);
         setVisible(true);
     }
 
-//    public static void main(String[] args) {
-//        // Create some data maps for each location
-//        LinkedHashMap<Integer, List<String>> dataMap1 = new LinkedHashMap<>();
-//        List<Double> data = new ArrayList<Double>(); 
-//        List<Double> data2 = new ArrayList<Double>(); 
-//        List<String> names = new ArrayList<String>(); 
-//        
-//        names.add("New York");
-//        names.add("London");
-//        
-//        data.add((double)12);
-//        data.add((double)22);
-//        data.add((double)42);
-//        data.add((double)20);
-//        data.add((double)10);
-//        data.add((double)6);
-//        
-//        data2.add((double)10);
-//        data2.add((double)22);
-//        data2.add((double)30);
-//        data2.add((double)20);
-//        data2.add((double)10);
-//        data2.add((double)3);
-//
-//        LinkedHashMap<String, Double> dataMap2 = new LinkedHashMap<>();
-//        dataMap2.put("London", null);
-//        dataMap2.put("London", 12.0);
-//        dataMap2.put("London", 18.0);
-//        dataMap2.put("London", 2.0);
-//        dataMap2.put("London", 30.0);
-//
-//        // Create the price table with the data maps
-//        new PriceTable(names, data,data2);
-//    }
 }
